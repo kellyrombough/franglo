10bf85ace19f63b871a488b4529e06dbf38c9a2c
Kelly Rombough <42428183+kellyrombough@users.noreply.github.com>
2018-11-24 19:56:07 -0500

    Added capability for multiple users

--- MySQLConnect.java
+++ MySQLConnect.java
@@ -10,24 +10,47 @@ public class MySQLConnect {
 			Statement stmt = conn.createStatement();
 			Statement stmt2 = conn.createStatement();
 			) {
-			//Execute a SQL SELECT query, the query result is returned in a 'ResultSet' object.
-			String strSelect = "SELECT * FROM DATA ORDER BY RAND()" ;
+			Scanner sc=new Scanner(System.in);
+			String username="";
+			String password="";
+			System.out.println("If you have an account already, type \"login\", if you would like to create a new account, type \"make account\":");
+			String s = sc.nextLine();
+			if (s.equals("make account")) username=makeAccount();
+			else{
+				//check that username and password pair exist in table of users
+				while (true) {
+					System.out.println("Please enter your username:");
+					username = sc.nextLine();
+					System.out.println("Please enter your password:");
+					password = sc.nextLine();
+					String checkLogin="SELECT * FROM users WHERE username=\"" + username + "\" AND password=\"" + password + "\"";
+					ResultSet rset2 = stmt.executeQuery(checkLogin);
+					if (rset2.next()) break;
+					System.out.println("invalid login credentials");
+				}
+			}
+			
+			//Select the columns containing the word list, in English and French
+			//Select the column containing each word's frequency ranking for the user that is logged in
+			String strSelect = "SELECT freq_" + username +", englishTr, frenchTr FROM wordList WHERE freq_" + username + "=0 ORDER BY RAND()";
+			System.out.println(strSelect);
 			ResultSet rset = stmt.executeQuery(strSelect);
 
-			//Process the ResultSet by scrolling the cursor forward via next()
-			Scanner sc=new Scanner(System.in);
 			boolean flag=false;
-			while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+			while(rset.next()) {   // Move the cursor to the next word, return false if no more words
 				String fr = rset.getString("frenchTr");
 				String en = rset.getString("englishTr");
 				String[] enArr=en.split(", ");
 				System.out.println("Enter the correct english translation for: " + fr );
 				String guess=sc.nextLine();
+				//check if user translated the word correctly
 				for (int i=0; i<enArr.length; i++) {
 					if (enArr[i].equalsIgnoreCase(guess)) {
+						//if right, increase the word's frequency ranking by one so that it will appear less often
+						//frequency ranking ranges from 0 (most frequent) to 4 (least frequent)
 						System.out.println("Correct!");
-						String strUpdate = ("UPDATE data SET ranking = ranking+1 WHERE TRIM(frenchTr)=\"" + fr + "\"");
-						System.out.println("The SQL query is: " + strUpdate);
+						String strUpdate = ("UPDATE wordList SET freq_" + username +" = freq_" + username + "+1 "
+											+ "WHERE TRIM(frenchTr)=\"" + fr + "\" AND " + "freq_" + username +"<4");
 						stmt2.executeUpdate(strUpdate);
 						flag=true;
 						break;
@@ -43,5 +66,47 @@ public class MySQLConnect {
 		catch(SQLException ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	public static String makeAccount() {
+		String newUser="";
+		String newPass="";
+		try (
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "username", "password");
+			Statement stmt = conn.createStatement();
+			) {
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				System.out.println("Enter your new username:");
+				newUser = sc.nextLine();
+				//check if username already exists
+				String checkExists = "SELECT * FROM users WHERE username=\"" + newUser + "\"";
+				ResultSet r = stmt.executeQuery(checkExists);
+				if (!r.next()) break;
+				System.out.println("that username is taken");
+			}
+			while (true) {
+				System.out.println("Enter your new password:");
+				newPass = sc.nextLine();
+				System.out.println("Confirm password:");
+				//make sure password is entered correctly
+				String newPassConfirm = sc.nextLine();
+				if (newPass.equals(newPassConfirm)) break;
+				System.out.println("Passwords didn't match, try again");
+			}
+			//add new user to table of users
+			String createAcc = "INSERT INTO users (username, password) VALUES (\"" + newUser + "\", \"" + newPass + "\")";
+			stmt.executeUpdate(createAcc);
+			
+			//add new column to wordlist table, setting the frequency ranking of every word to 0 for this new user
+			String newCol="ALTER TABLE wordlist ADD freq_"+newUser + " INT ";
+			stmt.executeUpdate(newCol);
+			String setZero = "UPDATE wordlist SET freq_"+newUser + "=0";
+			stmt.executeUpdate(setZero);
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return newUser;
 	}
 }
